c6bee9cf2e6332d05efa000233a6abaf9324dffa
wapjude <wapjude@gmail.com>
2017-03-14 15:55:40 +0000

    Add words count function and reverse String function

---
+++ app/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+	words: function(words){
+		
+    var value_count = {}; //to return the result
+
+    words.replace(/\n/g, " ").replace(/\t/g," ").replace(/ +/g, " ").split(" ").forEach(function(str){
+      
+        if(value_count[str] !== undefined){
+           //if value is in value_count  already just increment it
+           value_count[str] += 1;
+
+        }else{
+            //else add the key to the json file and set the value to be 1
+            value_count[str] = 1;
+
+        }
+
+    });
+
+  return value_count;
+
+	},
+	reverseString:function(word){
+	
+
+    	var st = word.split("").reverse().join(""); //split the values into array then reverse it 
+
+        return word === '' ? null : st === word ? true : st ;
+    	
+       
+	}
+}
